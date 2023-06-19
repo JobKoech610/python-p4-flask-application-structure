b46d3ae16dbbc688666f1816550d6c3039f0dccd
Job koech <jobkoech610@gmail.com>
2023-06-19 12:44:21 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -3,3 +3,21 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+
+
+# @app.route('/<string:hello>')
+# def user(hello):
+#     return f'<h1>{hello} there!</h1>'
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
